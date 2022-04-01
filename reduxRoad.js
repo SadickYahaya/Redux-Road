ea5fd70b169aeef0224826685094068a070c940e
Sadick Abubakar Yahaya <abubakar.yahaya@amalitech.org>
2022-04-01 18:21:52 +0000

    changes to state

--- reduxRoad.js
+++ reduxRoad.js
@@ -1,68 +1,65 @@
 let initialWagonState = {
-    supplies: 100,
-    distance: 0,
-    days: 0
-  };
-  
-  const reducer = (state = initialWagonState, action) =>{
-    switch (action.type){
-      case 'gather': {
-        return {...state, 
-        supplies: initialWagonState.supplies + 15,
-        distance: initialWagonState.distance,
-        days: initialWagonState.days + 1
-        }
-      }
-      case 'travel': {
-        return {...state, 
-        supplies: initialWagonState.supplies - (20),
-        distance: initialWagonState.distance + 10,
-        days: initialWagonState.days + 1
-        }
-      }
-      case 'tippedWagon': {
-        return {...state, 
-        supplies: initialWagonState.supplies - 30,
-        distance: initialWagonState.distance,
-        days: initialWagonState.days + 1
-        }
-      }
-      default: {
-        return state;
+  supplies: 100,
+  distance: 0,
+  days: 0
+};
+
+const wagonReducer = (state = initialWagonState, action) =>{
+  switch (action.type){
+    case 'gather': {
+      return {...state, 
+      supplies: state.supplies + 15,
+      days: state.days + 1
       }
     }
+    case 'travel': {
+      return {...state, 
+      supplies: state.supplies - 20,
+      distance: state.distance + 10,
+      days: state.days + action.payload
+      }
+    }
+    case 'tippedWagon': {
+      return {...state, 
+      supplies: state.supplies - 30,
+      days: state.days + 1
+      }
+    }
+    default: {
+      return state;
+    }
   }
-  
-  let state = reducer(undefined, {});
-  console.log(initialWagonState);
-  
-  initialWagonState = reducer(initialWagonState, 
-    {
-    type: 'travel', payload: 1
-    });
-    console.log(initialWagonState);
-  
-  initialWagonState = reducer(initialWagonState, 
-    {
-    type: 'gather'
-    });
-    console.log(initialWagonState);
-  
-  initialWagonState = reducer(initialWagonState, 
-    {
-    type: 'tippedWagon'
-    });
-    console.log(initialWagonState);
-  
-  initialWagonState = reducer(initialWagonState, 
-    {
-    type: 'travel',
-    payload: 3
-    });
-    console.log(initialWagonState);
-  
-  
-  
-  
-  
-  
+}
+
+let wagon = wagonReducer(undefined, {});
+console.log(wagon);
+
+wagon = wagonReducer(wagon, 
+  {
+  type: 'travel', payload: 1
+  });
+  console.log(wagon);
+
+wagon = wagonReducer(wagon, 
+  {
+  type: 'gather'
+  });
+  console.log(wagon);
+
+wagon = wagonReducer(wagon, 
+  {
+  type: 'tippedWagon'
+  });
+  console.log(wagon);
+
+wagon = wagonReducer(wagon, 
+  {
+  type: 'travel',
+  payload: 3
+  });
+  console.log(wagon);
+
+
+
+
+
